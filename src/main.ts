bbb5c6a7ea7afce577c69239cb98d0634a305617
sidhuiwnl <sidharthinfernal@gmail.com>
2026-02-06 17:33:19 +0000

    remove unused fn

--- src/main.ts
+++ src/main.ts
@@ -40,10 +40,7 @@ if (db) {
   v1Router.post("/notes", middlewareAuth(handlerNotesCreate));
 }
 
-function unused() {
-  // this function does nothing
-  // and is called nowhere
-}
+
 
 v1Router.get("/healthz", handlerReadiness);
 
